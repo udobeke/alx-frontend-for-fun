ea98dacf05fa469fb5a0a58362e868419f84a8ef
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-07-13 18:58:33 +0100

    Markdown to HTML

--- markdown2html.py
+++ markdown2html.py
@@ -5,6 +5,10 @@ A script that converts Markdown to HTML
 import sys
 import os
 import re
+import hashlib
+
+def remove_character(string, char):
+    return string.replace(char, '')
 
 if __name__ == '__main__':
 
@@ -27,6 +31,7 @@ if __name__ == '__main__':
         html_content = []
         md_content = [line[:-1] for line in file_1.readlines()]
         in_list = False  # Flag to track if currently inside a list
+        in_paragraph = False  # Flag to track if currently inside a paragraph
 
         for line in md_content:
             heading = re.split(r'#{1,6} ', line)
@@ -39,27 +44,43 @@ if __name__ == '__main__':
                     f'<h{h_level}>{heading[1]}</h{h_level}>\n'
                 )
                 in_list = False  # Reset list flag when a heading is encountered
-            elif line.startswith('* '):
+                in_paragraph = False  # Reset paragraph flag when a heading is encountered
+            elif line.startswith('- '):
                 # Start of an unordered list
                 if not in_list:
                     html_content.append('<ul>\n')
                     in_list = True
-                # Remove the '*' and space, and wrap the line in <li> tags
-                html_content.append(f'<li>{line[2:]}</li>\n')
-            elif line.startswith('1. '):
-                # Start of an ordered list
-                if not in_list:
-                    html_content.append('<ol>\n')
-                    in_list = True
-                # Remove the '1.' and space, and wrap the line in <li> tags
-                html_content.append(f'<li>{line[3:]}</li>\n')
+                    in_paragraph = False  # Reset paragraph flag when a list starts
+                # Remove the '-' and space, and wrap the line in <li> tags
+                line = line[2:]
+                line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)  # Replace ** with <b> tags
+                html_content.append(f'<li>{line}</li>\n')
             else:
                 # Regular text line
-                html_content.append(line)
+                if not in_paragraph:
+                    html_content.append('<p>\n')
+                    in_paragraph = True
+                    # Indent the line with 4 spaces
+                    html_content.append(f'    {line}\n')
+                else:
+                    # Indent the line with 8 spaces
+                    html_content.append(f'        {line}\n')
+                # Replace ** with <b> tags
+                line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
+                # Replace __ with <em> tags
+                line = re.sub(r'__(.*?)__', r'<em>\1</em>', line)
+                # Replace [[text]] with MD5 hash (lowercase) of text
+                line = re.sub(r'\[\[(.*?)\]\]', lambda match: hashlib.md5(match.group(1).encode()).hexdigest(), line)
+                # Remove ((text)) with 'c' removed from text (case insensitive)
+                line = re.sub(r'\(\((.*?)\)\)', lambda match: remove_character(match.group(1), 'c'), line)
+                html_content.append(f'    {line}\n')
 
         # Close the list if the last line was part of a list
         if in_list:
             html_content.append('</ul>\n')
+        # Close the paragraph if the last line was part of a paragraph
+        if in_paragraph:
+            html_content.append('</p>\n')
 
     with open(output_file, 'w', encoding='utf-8') as file_2:
         file_2.writelines(html_content)
